bfe375b9ec399caaa0f78098a3c955935bc450ea
Philipp Schafft <lion.leolix.org!lion>
2010-12-04 17:09:39 +0100

    initial code file

---
+++ lib/Audio/RoarAudio/VS.pm
@@ -0,0 +1,63 @@
+#      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2009-2010
+#
+#  This file is part of Audio::RPLD,
+#  a library to access the RoarAudio PlayList Daemon from Perl.
+#  See README for details.
+#
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3
+#  as published by the Free Software Foundation.
+#
+#  Audio::RPLD is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this software; see the file COPYING.gplv3.
+#  If not, write to the Free Software Foundation, 51 Franklin Street,
+#  Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+package Audio::RoarAudio::VS;
+
+use strict;
+use vars qw($VERSION @ISA);
+
+$VERSION     = 0.001;
+@ISA         = qw();
+
+1;
+
+=pod
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+  Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
+
+=head1 LICENSE
+
+      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2009-2010
+
+  This file is part of Audio::RPLD,
+  a library to access the RoarAudio PlayList Daemon from Perl.
+  See README for details.
+
+  This file is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  Audio::RPLD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this software; see the file COPYING.gplv3.
+  If not, write to the Free Software Foundation, 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301, USA.
+
+=cut
+
+#ll
